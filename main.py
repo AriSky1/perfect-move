6e2e8ad2476dba882fb3fb40f240abd7b31987de
Arina <105871709+AriSky1@users.noreply.github.com>
2024-06-17 17:48:45 +0200

    feat: video

--- main.py
+++ main.py
@@ -1,48 +1,33 @@
-
-
+from flask import Flask, render_template, Response
 import cv2
-import mediapipe as mp
 
-# Load the video
-video_path = 'video.mp4'
-cap = cv2.VideoCapture(video_path)
+app = Flask(__name__)
 
-# Initialize MediaPipe Pose model
-mp_pose = mp.solutions.pose
-pose = mp_pose.Pose()
+def generate_frames(camera_index):
+    cap = cv2.VideoCapture(camera_index)
+    while True:
+        success, frame = cap.read()
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
-# Process each frame of the video
-while cap.isOpened():
-    ret, frame = cap.read()
-    if not ret:
-        break
+@app.route('/video_feed1')
+def video_feed1():
+    return Response(generate_frames(0),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
-    # Convert the image to RGB
-    image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+@app.route('/video_feed2')
+def video_feed2():
+    return Response(generate_frames(1),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
-    # Make detection
-    results = pose.process(image)
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-    # Draw pose landmarks on the frame
-    if results.pose_landmarks:
-        # Example to draw a specific point (e.g., right shoulder)
-        # right_shoulder = results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_SHOULDER]
-        # cv2.circle(frame, (int(right_shoulder.x * frame.shape[1]), int(right_shoulder.y * frame.shape[0])), 5, (255, 0, 0), -1)
-
-        # Loop through all landmarks and draw them
-        for landmark in results.pose_landmarks.landmark:
-            # Draw only for arms and legs landmarks (adjust according to your needs)
-            if landmark.visibility > 0.5:
-                cx, cy = int(landmark.x * frame.shape[1]), int(landmark.y * frame.shape[0])
-                cv2.circle(frame, (cx, cy), 5, (255, 0, 0), -1)
-
-    # Display the frame
-    cv2.imshow('Dancer Pose Detection', frame)
-
-    # Break the loop if 'q' is pressed
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-# Release resources
-cap.release()
-cv2.destroyAllWindows()
+if __name__ == "__main__":
+    app.run(debug=True)
